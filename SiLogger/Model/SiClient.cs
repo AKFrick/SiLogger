56a08b1c00c6f9069fa60d69ab00cd33f5b93f3b
AKFrick <akfrick>
2021-02-15 20:34:11 +0500

    add siclint and message class

---
+++ SiLogger/Model/SiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiLogger.Model
+{
+    public class SiClient
+    {
+        public void Connect() { }
+        public void Disconnect() { }
+
+        public event Action<StateUpdates> StateUpdated;
+
+    }
+}
---
+++ SiLogger/Model/StateUpdates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiLogger.Model
+{
+    public class StateUpdates
+    {
+        public string MachineName { get; set; }
+        public string StateName { get; set; }
+        public int StateNum { get; set; }
+        public DateTime OccursTime { get; set; }
+
+        public StateUpdates(string StateName, int StateNum, string MachineName, DateTime OccursTime)
+        {
+            this.StateName = StateName;
+            this.StateNum = StateNum;
+            this.OccursTime = OccursTime;
+            this.MachineName = MachineName;
+        }        
+    }
+}
